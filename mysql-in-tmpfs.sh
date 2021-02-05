c8a9a96a133294e7e8e9ea82a0180636c80762d7
Dmitry Belyaev <d.belyaev@iblschool.ru>
2021-02-05 13:52:32 +0300

    Some updates

--- mysql-in-tmpfs.sh
+++ mysql-in-tmpfs.sh
@@ -35,7 +35,7 @@ readonly SCRIPT_NAME=$(basename $0)
 readonly SCRIPT_USAGE="/bin/bash $0"
 readonly SCRIPT_TIME_START=$(date +%s)
 
-function _err () {
+function err {
     local errmsg=$1
     local errcode=${2:-1}
     echo "ERROR: [$errcode] $errmsg"
@@ -43,48 +43,48 @@ function _err () {
 } >&2
 
 set -o pipefail
-function _iferr () {
+function iferr {
     local errcode=$?
-    (( $errcode != 0 )) && _err "" $errcode
+    (( $errcode != 0 )) && err "" $errcode
 }
 
-function _echo () {
+function echoDt {
     echo -e "`date '+%Y-%m-%d %H:%M:%S'`\t$@"
 }
 
-_echo "*** Going to run detached mysql instance based completely in RAM (tmpfs) ***"
+echoDt "*** Going to run detached mysql instance based completely in RAM (tmpfs) ***"
 
 # Must be run as root
-[[ $(whoami) != "root" ]] && _err "Must be run as root"
+[[ $(whoami) != "root" ]] && err "Must be run as root"
 
 # Check compatibility
-[[ $(which mysql) ]] || _err "Not found required binary: mysql"
-[[ $(which mysqld) ]] || _err "Not found required binary: mysqld"
-[[ $(which mysqladmin) ]] || _err "Not found required binary: mysqladmin"
-[[ $( mysql --version | grep -P '\s+Ver\s+8\.' ) ]] || _err "Required mysql version: 8.*"
+[[ $(which mysql) ]] || err "Not found required binary: mysql"
+[[ $(which mysqld) ]] || err "Not found required binary: mysqld"
+[[ $(which mysqladmin) ]] || err "Not found required binary: mysqladmin"
+[[ $( mysql --version | grep -P '\s+Ver\s+8\.' ) ]] || err "Required mysql version: 8.*"
 
 # Patch mysql apparmor if need
-[[ -e $MYSQL_APPARMOR_FILE ]] || _err "Mysql apparmor file not exists: $MYSQL_APPARMOR_FILE"
+[[ -e $MYSQL_APPARMOR_FILE ]] || err "Mysql apparmor file not exists: $MYSQL_APPARMOR_FILE"
 if [[ ! $( cat $MYSQL_APPARMOR_FILE | grep "$MYSQL_TMP_DATA_DIR" ) ]]; then
-    _echo "Patching mysql apparmor file: $MYSQL_APPARMOR_FILE"
+    echoDt "Patching mysql apparmor file: $MYSQL_APPARMOR_FILE"
     apparmOrigFile="/etc/apparmor/$(basename $MYSQL_APPARMOR_FILE).orig"
     cp -p $MYSQL_APPARMOR_FILE $apparmOrigFile
-    _echo "Original file saved to: $apparmOrigFile"
+    echoDt "Original file saved to: $apparmOrigFile"
     patch="\n"
     patch+="  # Mysql in tmpfs\n"
     patch+="  $MYSQL_TMP_DATA_DIR/ r,\n"
     patch+="  $MYSQL_TMP_DATA_DIR/** rwk,\n"
     patch+="  $MYSQL_TMP_ERR_LOG rw,\n"
     patch+="}"
-    sed -r -i "s~^\s*\}\s*$~$patch~" $MYSQL_APPARMOR_FILE; _iferr
-    systemctl reload apparmor.service; _iferr
+    sed -r -i "s~^\s*\}\s*$~$patch~" $MYSQL_APPARMOR_FILE; iferr
+    systemctl reload apparmor.service; iferr
 fi
 
 # Stop previous mysqld instance if exists
 [[ -e $MYSQL_TMP_PID_FILE ]] && pidMysqlSaved=$(cat $MYSQL_TMP_PID_FILE)
 if (( $pidMysqlSaved )); then
-    _echo "Found running mysqld temp instance ($pidMysqlSaved): stopping it"
-    kill -SIGTERM $pidMysqlSaved; _iferr
+    echoDt "Found running mysqld temp instance ($pidMysqlSaved): stopping it"
+    kill -SIGTERM $pidMysqlSaved; iferr
     terminated=0
     attempts=60
     for i in $(eval echo "{1..$attempts}"); do
@@ -93,20 +93,20 @@ if (( $pidMysqlSaved )); then
             break
         fi
         sleep 1
-        _echo "Waiting mysqld to stop [$((attempts-i))] ..."
+        echoDt "Waiting mysqld to stop [$((attempts-i))] ..."
     done
-    (( $terminated )) || _err "Failed to stop running mysqld temp instance (pid:$pidMysqlSaved)"
+    (( $terminated )) || err "Failed to stop running mysqld temp instance (pid:$pidMysqlSaved)"
 fi
 
-_echo "Creating mysql temp data dir"
-rm -rf $MYSQL_TMP_DATA_DIR; _iferr
-mkdir $MYSQL_TMP_DATA_DIR; _iferr
-chown $MYSQL_TMP_OWNER $MYSQL_TMP_DATA_DIR; _iferr
+echoDt "Creating mysql temp data dir"
+rm -rf $MYSQL_TMP_DATA_DIR; iferr
+mkdir $MYSQL_TMP_DATA_DIR; iferr
+chown $MYSQL_TMP_OWNER $MYSQL_TMP_DATA_DIR; iferr
 
-_echo "Initializing mysql temp data"
-mysqld --initialize --datadir=$MYSQL_TMP_DATA_DIR --disable-log-bin --log-error=$MYSQL_TMP_ERR_LOG; _iferr
+echoDt "Initializing mysql temp data"
+mysqld --initialize --datadir=$MYSQL_TMP_DATA_DIR --disable-log-bin --log-error=$MYSQL_TMP_ERR_LOG; iferr
 
-_echo "Running mysqld temp instance (host:$MYSQL_TMP_HOST port:$MYSQL_TMP_PORT)"
+echoDt "Running mysqld temp instance (host:$MYSQL_TMP_HOST port:$MYSQL_TMP_PORT)"
 mysqld \
 --port=$MYSQL_TMP_PORT \
 --socket=$MYSQL_TMP_SOCKET \
@@ -127,26 +127,26 @@ mysqld \
 --innodb_buffer_pool_size=1G \
 --innodb_buffer_pool_instances=4 \
 --innodb_io_capacity=2000 \
---daemonize=ON; _iferr
+--daemonize=ON; iferr
 
-_echo "Removing mysql temp generated root password"
+echoDt "Removing mysql temp generated root password"
 password=$(cat $MYSQL_TMP_ERR_LOG | grep -P 'A temporary password is generated for root@localhost' | tail -1 | awk '{ print $NF }')
-[[ $password ]] || _err "Failed to get mysql temp generated root password"
-mysqladmin --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -u root -p"$password" password ''; _iferr
+[[ $password ]] || err "Failed to get mysql temp generated root password"
+mysqladmin --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -u root -p"$password" password ''; iferr
 
-_echo "Creating mysql temp database and user"
-mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "CREATE DATABASE $MYSQL_TMP_DATABASE;"; _iferr
-mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "CREATE USER '$MYSQL_TMP_USER'@'%' IDENTIFIED BY '$MYSQL_TMP_PASSWORD';"; _iferr
-mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "GRANT ALL PRIVILEGES ON $MYSQL_TMP_DATABASE.* TO '$MYSQL_TMP_DATABASE'@'%';"; _iferr
-mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "FLUSH PRIVILEGES;"; _iferr
+echoDt "Creating mysql temp database and user"
+mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "CREATE DATABASE $MYSQL_TMP_DATABASE;"; iferr
+mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "CREATE USER '$MYSQL_TMP_USER'@'%' IDENTIFIED BY '$MYSQL_TMP_PASSWORD';"; iferr
+mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "GRANT ALL PRIVILEGES ON $MYSQL_TMP_DATABASE.* TO '$MYSQL_TMP_DATABASE'@'%';"; iferr
+mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT -e "FLUSH PRIVILEGES;"; iferr
 
-_echo "Time taken: $(( $(date +%s) - $SCRIPT_TIME_START )) sec"
+echoDt "Time taken: $(( $(date +%s) - $SCRIPT_TIME_START )) sec"
 
-_echo ""
-_echo "Created mysql temp instance in tmpfs"
-_echo "Connect: mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT --database=$MYSQL_TMP_DATABASE --user=$MYSQL_TMP_USER --password='$MYSQL_TMP_PASSWORD'"
-_echo "mysql_tmp_data_dir: $MYSQL_TMP_DATA_DIR"
-_echo "mysql_tmp_err_log: $MYSQL_TMP_ERR_LOG"
-_echo ""
-_echo "WARNING: ALL MYSQL TEMP DATA WILL BE LOST ON SERVER SHUTDOWN OR ACCIDENTAL POWER LOST"
-_echo ""
+echoDt ""
+echoDt "Created mysql temp instance in tmpfs"
+echoDt "Connect: mysql --host=$MYSQL_TMP_HOST --port=$MYSQL_TMP_PORT --database=$MYSQL_TMP_DATABASE --user=$MYSQL_TMP_USER --password='$MYSQL_TMP_PASSWORD'"
+echoDt "mysql_tmp_data_dir: $MYSQL_TMP_DATA_DIR"
+echoDt "mysql_tmp_err_log: $MYSQL_TMP_ERR_LOG"
+echoDt ""
+echoDt "WARNING: ALL MYSQL TEMP DATA WILL BE LOST ON SERVER SHUTDOWN OR ACCIDENTAL POWER LOST"
+echoDt ""
